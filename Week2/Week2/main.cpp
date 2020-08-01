49027d0b5937360a9f8b5d566451ac3042047d3b
leeyunhome <leeyunhome@daum.net>
2020-08-01 23:15:16 +0900

    add  vertex, color array

--- Week2/Week2/main.cpp
+++ Week2/Week2/main.cpp
@@ -38,6 +38,14 @@ static void cursor_position_callback(GLFWwindow* window, double xpos, double ypo
 	std::cout << xpos << " " << ypos << std::endl;
 }
 
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+	if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
+		//popup_menu();
+	{
+		std::cout << "button clicked" << std::endl;
+	}
+}
 
 int main()
 {
@@ -57,11 +65,15 @@ int main()
 	}
 
 	// callbacks here
-	glfwSetKeyCallback(window, key_callback);
+	/*glfwSetKeyCallback(window, key_callback);
 	glfwSetCursorPosCallback(window, cursor_position_callback);
+	glfwSetMouseButtonCallback(window, mouse_button_callback);*/
 
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
+
+	printf("%s\n", glGetString(GL_VERSION));
+
 	glClearColor(255.0/255.0, 153.0/255.0, 51.0/255.0, 1);	// white background
 	// default color for display buffer
 
@@ -74,7 +86,20 @@ int main()
 	glOrtho(-1, 1, -1 / aspect_ratio, 1 / aspect_ratio, -1.0, 1.0);
 
 	// You are going to study later in Viewing class.
+	float color[3][3]
+	{
+		{1.0, 0.0, 0.0},	// the color of the first vertex
+		{0.0, 1.0, 0.0},	// the color of the second vertex
+		{0.0, 0.0, 1.0},	// the color of the third vertex
+	};
+	float vertex[3][3]
+	{ 
+					{ 0.0, 0.0, 0.0 },	// first vertex
+					{ 0.5, 0.0, 0.0 },	// second vertex
+					{ 0.25, 0.5, 0.0},	// third vertex
+	};
 
+	int num_vertices = 3;
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
@@ -82,11 +107,20 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		// TODO: draw here
+		// In this example, we draw only one triangle
+		// this is for massive polygons.
+
 		glBegin(GL_TRIANGLE_FAN);
 
-		glColor3f(0.0, 0.0, 1.0);
-		//glVertex3f(0.0, 0.0, 0.0);	// == glVertex2f(0.0, 0.0); in 2D drawing
-		//
+		for (int v = 0; v < num_vertices; v++)
+		{
+			glColor3f(color[v][0], color[v][1], color[v][2]);
+			glVertex3f(vertex[v][0], vertex[v][1], vertex[v][2]);
+		}
+		//glColor3f(1.0, 0.0, 0.0);
+		////glVertex3f(0.0, 0.0, 0.0);	// == glVertex2f(0.0, 0.0); in 2D drawing
+		//glVertex3fv(first_vertex);
+
 		//glColor3f(0.0, 1.0, 0.0);
 		//glVertex3f(0.5, 0.0, 0.0);	// == glVertex2f(0.0, 0.0); in 2D drawing
 		//
@@ -95,21 +129,21 @@ int main()
 		
 		// center of polygonized circle
 		// center vertex
-		glVertex2f(circle_center_x, circle_center_y);
+		//glVertex2f(circle_center_x, circle_center_y);
 
-		const int num_triangles = 1000;
-		const float dtheta = 2.0 * 3.141592 / (float)num_triangles;
-		const float radius = 0.5;
+		//const int num_triangles = 1000;
+		//const float dtheta = 2.0 * 3.141592 / (float)num_triangles;
+		//const float radius = 0.5;
 
-		float theta = 0.0;
-		for (int i = 0; i <= num_triangles; i++, theta += dtheta)
-		//for (float theta = 0.0; theta < 2.0 * 3.141592; theta += dtheta)
-		{
-			const float x = radius * cos(theta) + circle_center_x;
-			const float y = radius * sin(theta) + circle_center_y;
-			glVertex2f(x, y);	// == glVertex2f(0.0, 0.0); in 2D drawing
+		//float theta = 0.0;
+		//for (int i = 0; i <= num_triangles; i++, theta += dtheta)
+		////for (float theta = 0.0; theta < 2.0 * 3.141592; theta += dtheta)
+		//{
+		//	const float x = radius * cos(theta) + circle_center_x;
+		//	const float y = radius * sin(theta) + circle_center_y;
+		//	glVertex2f(x, y);	// == glVertex2f(0.0, 0.0); in 2D drawing
 
-		}
+		//}
 		glEnd();
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
