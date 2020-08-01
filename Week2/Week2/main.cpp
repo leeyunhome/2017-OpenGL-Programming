b33ae45358969655b316eede0ca377f7b331bcd5
leeyunhome <leeyunhome@daum.net>
2020-08-01 22:25:33 +0900

    find position of my cursor

--- Week2/Week2/main.cpp
+++ Week2/Week2/main.cpp
@@ -13,6 +13,32 @@
 const int width_window = 640; //*2;
 const int height_window = 480; //*2;
 
+float circle_center_x = 0.0;
+float circle_center_y = 0.0;
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS)
+		//activate_airship();
+		std::cout << "Key pressed" << std::endl;
+	if (key == GLFW_KEY_LEFT && action == GLFW_PRESS)
+		//activate_airship();
+	{
+		circle_center_x -= 0.05;
+	}
+	if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS)
+		//activate_airship();
+	{
+		circle_center_x += 0.05;
+	}
+}
+
+static void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	std::cout << xpos << " " << ypos << std::endl;
+}
+
+
 int main()
 {
 	GLFWwindow* window = nullptr;
@@ -31,6 +57,8 @@ int main()
 	}
 
 	// callbacks here
+	glfwSetKeyCallback(window, key_callback);
+	glfwSetCursorPosCallback(window, cursor_position_callback);
 
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
@@ -55,6 +83,7 @@ int main()
 
 		// TODO: draw here
 		glBegin(GL_TRIANGLE_FAN);
+
 		glColor3f(0.0, 0.0, 1.0);
 		//glVertex3f(0.0, 0.0, 0.0);	// == glVertex2f(0.0, 0.0); in 2D drawing
 		//
@@ -65,7 +94,8 @@ int main()
 		//glVertex3f(0.25, 0.5, 0.0);	// == glVertex2f(0.0, 0.0); in 2D drawing
 		
 		// center of polygonized circle
-		glVertex2f(0.0, 0.0);
+		// center vertex
+		glVertex2f(circle_center_x, circle_center_y);
 
 		const int num_triangles = 1000;
 		const float dtheta = 2.0 * 3.141592 / (float)num_triangles;
@@ -75,8 +105,8 @@ int main()
 		for (int i = 0; i <= num_triangles; i++, theta += dtheta)
 		//for (float theta = 0.0; theta < 2.0 * 3.141592; theta += dtheta)
 		{
-			const float x = radius * cos(theta);
-			const float y = radius * sin(theta);
+			const float x = radius * cos(theta) + circle_center_x;
+			const float y = radius * sin(theta) + circle_center_y;
 			glVertex2f(x, y);	// == glVertex2f(0.0, 0.0); in 2D drawing
 
 		}
